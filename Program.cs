7ea955f07cd1a38ed53c97d6ed889c571f38e361
Antonio <abamorim36@gmail.com>
2024-03-17 11:31:12 -0300

    Creating a Regex

--- Program.cs
+++ Program.cs
@@ -1,2 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+using System.Text.RegularExpressions;
+
+var regexMatchNameAndPort = new Regex(@"([^\s:]:(\d+)").Match(string.Join(' ', args));
+
+var name = regexMatchNameAndPort.Success ? 
